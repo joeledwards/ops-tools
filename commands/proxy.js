5367b583e7fe53a88e8c8d5d286fe93a2bc56e67
Joel Edwards <joeledwards@gmail.com>
2018-02-07 13:09:45 -0700

    feat: initial stab at proxy

--- commands/proxy.js
+++ commands/proxy.js
@@ -1,17 +1,51 @@
 module.exports = {
-  command: 'proxy <url>',
+  command: 'proxy <target-url>',
   desc: 'proxies to a remote URL and logs traffic for debugging',
   builder,
   handler
 }
 
 function builder (yargs) {
+  yargs
+  .option('bind-port', {
+    type: 'number',
+    desc: 'port on which the proxy server should listen',
+    default: 8118
+  })
+  .option('bind-host', {
+    type: 'string',
+    desc: 'host on which the proxy server should listen',
+    default: '0.0.0.0'
+  })
 }
 
-function handler ({url}) {
+function handler ({bindPort, bindHost, targetUrl}) {
   require('log-a-log')()
 
-  // const httpProxy = require('http-proxy')
+  const {blue, gray, green, orange, purpe, red, yellow} = require('@buzuli/color')
+  const http = require('http')
+  const httpProxy = require('http-proxy')
 
-  console.log('Not implemented yet. Check back later!')
+  const proxy = httpProxy.createProxy({
+  })
+
+  proxy.on('error', error => {
+    console.error(error)
+  })
+
+  let nextId = 0
+  const server = http.createServer((req, res) => {
+    const id = nextId++
+    console.info(`[${orange(id)}] ${orange(req.method)} ${green(req.url)}`)
+
+    proxy.web(req, res, {
+      target: targetUrl,
+      "start": "node bin/ops.js",
+    })
+  })
+
+  server.listen(bindPort, bindHost, () => {
+    console.info(`${bindHost}:${bindPort} => ${targetUrl}`)
+    console.info('Proxy server listening...')
+  })
 }
