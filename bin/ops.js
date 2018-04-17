32ba514ed924f46c876fb2ad9ebf43cf068ba5ed
Joel Edwards <joeledwards@gmail.com>
2018-04-16 19:59:15 -0600

    feat: added update notification (checks hourly)

--- bin/ops.js
+++ bin/ops.js
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
 const yargs = require('yargs')
 
+function updateCheck () {
+  const updateNotifier = require('update-notifier')
+  const pkg = require('../package.json')
+  const notifier = updateNotifier({pkg, updateCheckInterval: 3600000})
+
+  if (notifier.update) {
+    notifier.notify()
+  }
+}
+
 function run () {
   return yargs
+    .exitProcess(false)
     .commandDir('../commands')
     .demandCommand()
-    .help()
+    .fail(() => {
+      yargs.showHelp()
+
+      // only perform the update check when showing help
+      updateCheck()
+    })
     .argv
 }
 
