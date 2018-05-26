d40008fa396402815bcc7c893b8b6cd921a806c5
Joel Edwards <joeledwards@gmail.com>
2018-05-25 21:44:34 -0600

    feat: added s3-buckets command

---
+++ commands/s3-buckets.js
@@ -0,0 +1,28 @@
+module.exports = {
+  command: 's3-buckets',
+  desc: 'list out region S3 buckets',
+  handler
+}
+
+function handler () {
+  const buzJson = require('@buzuli/json')
+  const c = require('@buzuli/color')
+  const s3 = require('../lib/aws').s3()
+
+  s3.listBuckets()
+    .then(result => {
+      const buckets = result.Buckets.map(({Name}) => c.blue(Name))
+      const count = buckets.length
+      console.log(buckets.join('\n'))
+      console.log(c.green(
+        `Listed ${c.orange(count)} buckets for region ${c.yellow(s3.aws.region)}`
+      ))
+    })
+    .catch(error => {
+      console.error(error)
+      console.error(red(
+        `Error listing buckets in ${yellow(s3.aws.region)}: details above :point_up:`
+      ))
+      process.exit(1)
+    })
+}
