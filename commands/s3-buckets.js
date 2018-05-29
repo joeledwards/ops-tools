70f0a05d3a166f1bd2acd9f8731c5e4595435fc1
Joel Edwards <joeledwards@gmail.com>
2018-05-29 07:50:17 -0600

    feat: using color pool in s3-bucket to make the regions visually distinct

--- commands/s3-buckets.js
+++ commands/s3-buckets.js
@@ -8,21 +8,40 @@ function handler () {
   const buzJson = require('@buzuli/json')
   const c = require('@buzuli/color')
   const s3 = require('../lib/aws').s3()
+  const r = require('ramda')
 
   s3.listBuckets()
-    .then(result => {
-      const buckets = result.Buckets.map(({Name}) => c.blue(Name))
+    .then(async ({Buckets: buckets}) => {
+      const regionDecor = c.pool()
+
+      console.log(
+        (await Promise.all(
+          buckets.map(({Name: bucket}) => {
+            return s3.getBucketLocation({Bucket: bucket})
+              .then(({LocationConstraint: region}) => {
+                return {
+                  bucket,
+                  region: r.isEmpty(region) ? 'us-east-1' : region
+                }
+              })
+          })
+        ))
+        .map(({bucket, region}) => {
+           return `  [${regionDecor(region)}] ${c.yellow(bucket)}`
+        })
+        .join('\n')
+      )
+
       const count = buckets.length
-      console.log(buckets.join('\n'))
       console.log(c.green(
         `Listed ${c.orange(count)} buckets for region ${c.yellow(s3.aws.region)}`
       ))
     })
     .catch(error => {
       console.error(error)
-      console.error(red(
-        `Error listing buckets in ${yellow(s3.aws.region)}: details above :point_up:`
-      ))
+      console.error(c.red(c.emoji.inject(
+        `Error listing buckets in ${c.yellow(s3.aws.region)}: details above :point_up:`
+      )))
       process.exit(1)
     })
 }
